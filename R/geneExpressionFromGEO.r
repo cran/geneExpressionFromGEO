caeb56643ef1c8d3af06e197e37e6ab93a938073
Davide Chicco <davidechicco@davidechicco.it>
2021-11-06 23:30:02 +0000

    version 0.8

--- R/geneExpressionFromGEO.r
+++ R/geneExpressionFromGEO.r
@@ -1,5 +1,15 @@
 
 
+#' Function that returns numeric values with 2 decimal numbers.
+#' 
+#' @param x input numeric value with N decimal numbers.
+#' @return a numeric value with 2 decimal numbers.
+#' @examples
+#' aaa <- dec_two(8.31232)
+ dec_two <- function(x) {
+  return (format(round(x, 2), nsmall = 2));
+}
+
 #' Function that reads in a URL to check and verifies if it exists (function written by Janko Thyson @rappster and taken from https://stackoverflow.com/a/12195574 )
 #' 
 #' @param url the URL of a webpage
